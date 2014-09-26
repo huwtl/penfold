fc43d7feb5145af51e1b06c6855b871735bdf3e0
Huw Lewis <hlewis@qmetric.co.uk>
2014-09-26 09:48:05 +0100

    fix to concurrency issue with regards to auto_inc column race condition

--- src/main/scala/com/qmetric/penfold/support/Retry.scala
+++ src/main/scala/com/qmetric/penfold/support/Retry.scala
@@ -1,14 +1,36 @@
 package com.qmetric.penfold.support
 
 import scala.util.{Success, Try}
+import scala.concurrent.duration.Duration
 
 object Retry {
   @annotation.tailrec
-  def retry[T](n: Int)(fn: => T): util.Try[T] = {
-    Try { fn } match {
+  def retry[T](retries: Int)(func: => T): util.Try[T] = {
+    Try { func } match {
       case x: Success[T] => x
-      case _ if n > 1 => retry(n - 1)(fn)
+      case _ if retries > 1 => retry(retries - 1)(func)
       case f => f
+    }
+  }
+
+  @annotation.tailrec
+  def retryUntilSome[T](retries: Int, interval: Duration)(func: => Option[T]): Option[T] = {
+    func match {
+      case None if retries > 1 => {
+        sleep(interval.toMillis)
+        retryUntilSome(retries - 1, interval)(func)
+      }
+      case None => None
+      case some => some
+    }
+  }
+
+  private def sleep(millis: Long)
+  {
+    try {
+      Thread.sleep(millis)
+    } catch {
+      case e: InterruptedException => Thread.currentThread().interrupt()
     }
   }
 }
---
+++ src/test/scala/com/qmetric/penfold/support/RetryTest.scala
@@ -0,0 +1,43 @@
+package com.qmetric.penfold.support
+
+import org.specs2.mutable.Specification
+import org.specs2.mock.Mockito
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit._
+
+class RetryTest extends Specification with Mockito {
+
+  "should not retry when result is Some(x) on first attempt" in {
+    val service = mock[TestService]
+    service.execute returns Some("ok")
+
+    val result = Retry.retryUntilSome[String](2, FiniteDuration(3, MILLISECONDS)){service.execute}
+
+    result must beSome("ok")
+    there was one(service).execute
+  }
+
+  "should retry until result becomes Some(x)" in {
+    val service = mock[TestService]
+    service.execute returns None thenReturns Some("ok")
+
+    val result = Retry.retryUntilSome[String](2, FiniteDuration(3, MILLISECONDS)){service.execute}
+
+    result must beSome("ok")
+    there was two(service).execute
+  }
+
+  "should return none after max retries waiting for result to become Some(x)" in {
+    val service = mock[TestService]
+    service.execute returns None
+
+    val result = Retry.retryUntilSome[String](2, FiniteDuration(1, MILLISECONDS)){service.execute}
+
+    result must beNone
+    there was two(service).execute
+  }
+
+  trait TestService {
+    def execute: Option[String]
+  }
+}
